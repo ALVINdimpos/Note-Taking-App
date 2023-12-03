12c2b2fa9c9d0c9ac5e071cdc35502cd8900065c
ALVIN CODER <ALVINdimpos>
2023-12-03 22:29:42 +0200

    fx:db: connect ETIMEDOUT

--- src/config/config.js
+++ src/config/config.js
@@ -1,31 +1,19 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+
+// CONFIGURE DOTENV
+dotenv.config();
+
+// LOAD ENV VARIABLES
+const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } = process.env;
+
+// EXPORT CONFIG
 module.exports = {
   development: {
-    username: 'root',
-    password: 'bizcotap',
-    database: 'bizcotap_new',
-    host: '127.0.0.1',
-    port: 3306,
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: false,
-    },
-  },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: false,
-    },
-  },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    port: DB_PORT,
+    host: DB_HOST,
     dialect: 'mysql',
     dialectOptions: {
       ssl: false,
